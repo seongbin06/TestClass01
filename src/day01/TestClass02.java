b43e1ab83ae7933fd95fd3042bf1f5af5ae8abb1
csb <csb@csb.com>
2023-06-12 15:37:57 +0900

    최종

---
+++ src/day01/TestClass02.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class TestClass02 {
+public static void main(String[] args) {
+	/*
+	 escape
+	 - 문자열 안에서 특수한 기능을 한다.
+	 \n : 엔터값과 비슷하다.
+	 \t : tab크기만큼 오른쪽으로 커서 이동
+	 \" : "(큰 따옴표)하나 표현
+	 \\ : 역슬래시 하나 표현
+	 */
+	System.out.println("안\n녕\n하\n세\n요");
+	System.out.println("1\t1234567\t13245678\t1");
+	System.out.println("이름\t나이");
+	System.out.println("홍길동\t20살");
+	
+	System.out.println("   \"  \\");
+}
+}
---
+++ src/day01/TestClass03.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class TestClass03 {
+public static void main(String[] args) {
+	System.out.println(100+100);
+	System.out.println(100.123+100.123);
+	
+	System.out.println("안녕 "+" 하세요");
+	System.out.println("100"+100);
+	System.out.println("백"+100);
+	
+	System.out.println("안녕"+(100+300));
+}
+}
