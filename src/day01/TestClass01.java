dc6c56f52afbc2422be4b0275cce764812b917de
csb <csb@csb.com>
2023-06-12 14:27:22 +0900

    TestClass01

---
+++ src/day01/TestClass01.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class TestClass01 {
+	public static void main(String[] args) {
+		//괄호 안의 내용을 출력해주는 기능
+		// ; : 세미콜론이며 문장의 마지막에는 꼭 있어야한다.
+		System.out.println(100);
+		System.out.println("안녕하세요");
+		
+		System.out.print( "1.안녕" );
+		System.out.print( "2.안녕" );
+		System.out.print( "3.안녕" );
+	}
+}
